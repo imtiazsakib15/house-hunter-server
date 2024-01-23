8ce6b65af0cbb140818700dbe6a0b652d36dc302
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-01-24 00:23:12 +0600

    Password encrypted before save

--- src/api/v1/authentication/controllers/postUser.js
+++ src/api/v1/authentication/controllers/postUser.js
@@ -1,14 +1,16 @@
 const User = require("../../../../models/authentication/userSchema");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 const postUser = async (req, res) => {
-  const newUser = new User(req.body);
+  const { email, password, name, phone, role } = new User(req.body);
+  const oldUser = await User.findOne({ email });
+  if (oldUser) return res.status(401).json({ error: "User already exists!" });
 
-  //   const encryptedPassword = await bcrypt.hash(password, 10);
+  const encryptedPassword = await bcrypt.hash(password, 10);
 
-  //   const newUser = { email, password: encryptedPassword, name, phone, role };
+  const newUser = { email, password: encryptedPassword, name, phone, role };
 
-  const result = await newUser.save();
+  const result = await User.create(newUser);
   res.status(201).send(result);
 };
 
--- src/routes/v1/authentication/index.js
+++ src/routes/v1/authentication/index.js
@@ -1,6 +1,7 @@
 const postUser = require("../../../api/v1/authentication/controllers/postUser");
 const router = require("express").Router();
 
+// Post user info to database api
 router.post("/register", postUser);
 
 module.exports = router;
